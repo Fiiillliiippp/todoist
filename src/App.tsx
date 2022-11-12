fdbb96e4cfb1c83b6e05df06b203ab91ac086703
matejbb <matej.mb97@gmail.com>
2022-11-12 21:14:06 +0100

    Comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Container from './components/Container';
-import TheNavigation from './components/navigation/TheNavigation';
-import Router from './components/routes/Router';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Container from "./components/Container"; //Container by nemusel byť v component
+import TheNavigation from "./components/navigation/TheNavigation"; //naming - bez The
+import Router from "./components/routes/Router";
 
 function App() {
   return (
     <BrowserRouter>
       <Container>
-        {params => (
-          <div className='App'>
+        {(params) => (
+          <div className="App">
             <TheNavigation />
             <Router />
           </div>
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,64 +1,73 @@
-import { useState } from 'react';
-import { TodoLists } from '../types/types';
-import { Provider } from './Context';
+import { useState } from "react";
+import { TodoLists } from "../types/types";
+import { Provider } from "./Context"; //Aj kontext by mohol byť mimo
 
 export type AppState = {
   lists: TodoLists;
   onAddNewList: (val: string, todoVal: string) => void;
   onEditTitle: (idList: number, value: string, todosValue: string) => void;
-  onTodoDone: (idList: number, checked: boolean) => void
+  onTodoDone: (idList: number, checked: boolean) => void;
 };
 
 type Props = {
   children: (props: AppState) => JSX.Element;
 };
+
 const Container = ({ children }: Props) => {
   const [todoLists, setTodoLists] = useState<TodoLists>([]);
 
   const handleAddNewTodoList = (val: string, todoVal: string) => {
-    setTodoLists(prevList => [
+    setTodoLists((prevList) => [
+      //toto treba prerobiť, pozri ako to máš vracať podľa toho čo sme robili
       ...prevList,
       { id: prevList.length + 1, title: val, todos: todoVal },
     ]);
   };
 
   const handleEditTitle = (
-    idList: number,
-    value: string,
+    idList: number, //naming
+    value: string, //ak si zmenis naming vies to pouzit ako key/value
     todosValue: string
   ) => {
-    setTodoLists(prevList =>
-      prevList.map(list => {
+    setTodoLists((prevList) =>
+      prevList.map((list) => {
+        //naming
         if (list.id === idList) {
-          if(value.length !== 0 && todosValue.length !== 0) {
-            return { ...list, title: value, todos: todosValue};
-          }else if (value.length !== 0 && todosValue.length === 0) {
-            return { ...list, title: value};
+          //naming
+          if (value.length !== 0 && todosValue.length !== 0) {
+            return { ...list, title: value, todos: todosValue };
+          } else if (value.length !== 0 && todosValue.length === 0) {
+            return { ...list, title: value };
           } else if (value.length === 0 && todosValue.length !== 0) {
             return { ...list, todos: todosValue };
-          }else {
-            return { ...list};
+          } else {
+            return { ...list };
           }
         }
+
         return list;
       })
     );
-  };
+  }; //nad tymto sa este zamysliet
 
   const handleTodoIsDone = (idList: number, checked: boolean) => {
-    setTodoLists(prevList => 
-      prevList.filter(list => {
-        if(list.id === idList && checked === true) {
-          return list.id !== idList
-        }return list
-      }))
-  }
+    //naming
+    setTodoLists((prevList) =>
+      prevList.filter((list) => {
+        if (list.id === idList && checked === true) {
+          //booleany sa takto necheckuju
+          return list.id !== idList; //vraciaš boolean
+        }
+        return list; //vraciaš list
+      })
+    );
+  }; //filtrovanie prerobit podla toho co sme sa uz bavili, lebo toto funguje omylom
 
   const appState: AppState = {
     lists: todoLists,
     onAddNewList: handleAddNewTodoList,
     onEditTitle: handleEditTitle,
-    onTodoDone: handleTodoIsDone
+    onTodoDone: handleTodoIsDone,
   };
 
   return <Provider value={appState}>{children(appState)}</Provider>;
