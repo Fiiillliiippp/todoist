4a6b1809f483966bd86443b89739630b65e8a55f
jakubbt <jakubbt.dev@gmail.com>
2022-11-14 18:05:57 +0100

    Suggestions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,59 +10,61 @@ const Home = () => {
   const [newTodoItem, setNewTodoItem] = useState('');
   const [addingList, setAddingList] = useState<boolean>(false);
 
-  const handleTitleChange = (e: string) => {
-    setNewListTitle(e);
+  const handleTitleChange = (title: string) => {
+    setNewListTitle(title);
   };
 
-  const handleTodoChange = (e: string) => {
-    setNewTodoItem(e)
-  }
   return (
     <div>
-      {/* Todo Item */}
       {lists.map(list => (
         <Todo key={list.id} list={list} />
       ))}
-      {/* adding todo */}
       {!addingList && (
         <div>
-          <Button variant="text" onClick={() => setAddingList(true)}>Add Todo</Button>
+          <Button variant='text' onClick={() => setAddingList(true)}>
+            Add List
+          </Button>
         </div>
       )}
       {addingList && (
         <Box
-        sx={{border: "1px solid black", borderRadius: 2 ,width: 250, justifyContent: "center", margin: "auto", marginTop: 2}}>
-        <Input
-        sx={{margin: 1}}
-          type='textarea'
-          placeholder='Todo Name'
-          value={newListTitle}
-          onChange={e => handleTitleChange(e.target.value)}
-          autoFocus
-        /> <br />
-        <Input
-        sx={{fontSize: 14}}
-          type='textarea'
-          placeholder='Todo Item'
-          value={newTodoItem}
-          onChange={e => handleTodoChange(e.target.value)}
-        /> <br />
-        <Button
-        sx={{margin: 1}}
-        variant="text"
-          onClick={() => {
-            if (newListTitle.length !== 0) {
-              onAddNewList(newListTitle, newTodoItem);
-              setNewListTitle('');
-              setNewTodoItem('');
-              setAddingList(false);
-            }
+          sx={{
+            border: '1px solid black',
+            borderRadius: 2,
+            width: 250,
+            justifyContent: 'center',
+            margin: 'auto',
+            marginTop: 2,
           }}
         >
-          Add
-        </Button>
-        <Button variant="text" onClick={() => setAddingList(false)}>Cancel</Button>
-      </Box>
+          <Input
+            sx={{ margin: 1 }}
+            type='textarea'
+            placeholder='Todo List Name'
+            value={newListTitle}
+            onChange={e => handleTitleChange(e.target.value)}
+            autoFocus
+          />
+          <br />
+          <br />
+          <Button
+            sx={{ margin: 1 }}
+            variant='text'
+            onClick={() => {
+              if (newListTitle.length !== 0) {
+                onAddNewList(newListTitle, newTodoItem);
+                setNewListTitle('');
+                setNewTodoItem('');
+                setAddingList(false);
+              }
+            }}
+          >
+            Add
+          </Button>
+          <Button variant='text' onClick={() => setAddingList(false)}>
+            Cancel
+          </Button>
+        </Box>
       )}
     </div>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -5,18 +5,30 @@
 // }
 
 export type TodoTag = {
-  id: number,
-  tagText: string
-}
+  id: number;
+  tagText: string;
+};
+
+export type Todo = {
+  id: number;
+  title: string;
+  desc: string;
+  tags: TodoTag[];
+  done: boolean;
+};
 
 export type TodoList = {
-  id: number,
-  title: string,
-  todo: string,
-  priority: number,
-  todoTag: TodoTag[]
+  id: number;
+  title: string;
+  todos: Todo[];
+  priority: number;
+  listDone: boolean;
+  listTags: {
+    id: number;
+    text: string;
+  }[];
   //priority from height to low---1,2,3,4
-}
+};
 
-export type TodoTags = TodoTag[]
-export type TodoLists = TodoList[]
+export type TodoTags = TodoTag[];
+export type TodoLists = TodoList[];
