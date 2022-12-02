0168a97dd98780333e924367a7992f67a15283cb
Filip B <filipbrady@gmail.com>
2022-12-02 12:43:18 +0100

    nav styling

--- src/components/navigation/TheNavigation.tsx
+++ src/components/navigation/TheNavigation.tsx
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom"
-import { routes } from "../../data/routes"
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { routes } from '../../data/routes';
 
 const TheNavigation = () => {
-  return(
-    <nav>
-      <Link to={`${routes.home}`}>Home</Link>
-      <Link to={`${routes.calendar}`}>Calendar</Link>
-      <Link to={`${routes.filters}`}>Filters</Link>
-      <Link to={`${routes.projects}`}>Projects</Link>
+  return (
+    <nav style={{margin: "15px"}}>
+      <Button variant='outlined' sx={{marginX: 1, padding: "4px 8px"}} >
+        <Link className="navLink" to={`${routes.home}`}>Home</Link>
+      </Button>
+      <Button variant='outlined' sx={{padding: "4px 8px"}} >
+        <Link className="navLink" to={`${routes.calendar}`}>Calendar</Link>
+      </Button>
+      <Button variant='outlined' sx={{marginX: 1, padding: "4px 8px"}} >
+        <Link className="navLink" to={`${routes.filters}`}>Filters</Link>
+      </Button>
+      <Button variant='outlined' sx={{padding: "4px 8px"}} >
+        <Link className="navLink" to={`${routes.projects}`}>Projects</Link>
+      </Button>
     </nav>
-  )
-}
+  );
+};
 
-export default TheNavigation
+export default TheNavigation;
